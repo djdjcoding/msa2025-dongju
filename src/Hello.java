7de2c02610683b8a7338ea9cdab33b6be89e8533
djdjcoding <dongju12100@gmail.com>
2025-03-14 17:35:19 +0900

    3. eclipse에서 또 다시 수정

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요");
 		System.out.println("안녕하세요 : github에서 수정");
+		System.out.println("안녕하세요 : eclipse에서 수정");
 	}
 }
